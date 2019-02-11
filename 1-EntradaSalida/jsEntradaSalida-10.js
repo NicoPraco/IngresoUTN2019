b47a524cbe56b82d415f7b39068446ac40ff465f
NicoPraco <47321381+NicoPraco@users.noreply.github.com>
2019-02-11 12:34:07 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
-	
+	var importe;
+    var descuento;
+    var precioFinal;
+
+    importe = parseFloat(document.getElementById("importe").value);
+
+    descuento = importe * 25 / 100
+
+    precioFinal = importe - descuento;
+
+    document.getElementById("resultado").value = precioFial;
+
+    alert("Se aplico un descuento de $" + descuento);
+
 }
